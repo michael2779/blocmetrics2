bbb69eeac2180feab742d9c5e4ceaf022a0f035a
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-08-21 16:05:58 -0700

    finished api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   resources :registered_applications
   devise_for :users
-  root 'welcome#index'
+  root 'registered_applications#index'
 
 
 end
